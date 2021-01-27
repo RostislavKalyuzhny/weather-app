c0e134c0a5e505a592c75b79c5cfed06ac1354c2
Rostislav Kalyuzhny <rkalyuzhniyy@gmail.com>
2021-01-28 00:30:54 +0200

    added logic for search panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,79 @@
+import { useState } from 'react';
+
 import './App.scss';
 
 import SearchPanel from './components/SearchPanel/SearchPanel';
 
 const App = () => {
-  return (
-    <div className="app">
-      <div className="container">
 
-      	<SearchPanel/>
+	const cities = [
+                    {
+                        "name": "Александрия",
+                        "lat": "48.6666667",
+                        "lng": "33.1166667"
+                    },
+                    {
+                        "name": "Бобринец",
+                        "lat": "48.050861",
+                        "lng": "32.16589"
+                    },
+                    {
+                        "name": "Гайворон",
+                        "lat": "48.3333333",
+                        "lng": "29.8666667"
+                    },
+                    {
+                        "name": "Долинская",
+                        "lat": "48.1108333",
+                        "lng": "32.7647222"
+                    },
+                    {
+                        "name": "Знаменка",
+                        "lat": "48.7166667",
+                        "lng": "32.6666667"
+                    },
+                    {
+                        "name": "Кировоград",
+                        "lat": "48.508389",
+                        "lng": "32.264801"
+                    },
+                    {
+                        "name": "Малая Виска",
+                        "lat": "48.65",
+                        "lng": "31.6333333"
+                    },
+                    {
+                        "name": "Новомиргород",
+                        "lat": "48.7833333",
+                        "lng": "31.65"
+                    },
+                    {
+                        "name": "Новоукраинка",
+                        "lat": "48.3155556",
+                        "lng": "31.5269444"
+                    },
+                    {
+                        "name": "Светловодск",
+                        "lat": "49.0502778",
+                        "lng": "33.2419444"
+                    }
+                ];
 
-      </div>	
-    </div>
-  );
+    const [currentCity, setCurrentCity] = useState(null);
+
+    const findCityObj = (name) => {
+    	setCurrentCity( cities.find(city => city.name === name) );
+    }
+
+	return (
+		<div className="app">
+			<div className="container">
+
+				<SearchPanel options={cities} onSelected={ (cityName) => findCityObj(cityName) }/>
+
+			</div>	
+		</div>
+	);
 }
 
 export default App;
--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -1,24 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './SearchPanel.scss';
 
-const SearchPanel = () => {
+const SearchPanel = ({ options : cities, onSelected }) => {
 
-	const [option, setOption] = useState('volvo');
+	const [option, setOption] = useState(cities[0].name);
 
-	const handleChange = (event) => {
+	useEffect(() => onSelected(option), [option]);
+	
+	const handleChange = async (event) => {
 	    setOption(event.target.value);
   	};
 
-  	console.log(option);
+  	const optionCities = cities.map((city, index) => {
+  		return (
+  			<option key={ index } 
+  					value={ city.name }>
+  					{ city.name }
+  			</option>
+  		)
+  	});
 
 	return (
 		<div className="search-panel">
-		  	<select defaultValue={option} onChange={handleChange} >
-				<option value="volvo">Volvo</option>
-				<option value="saab">Saab</option>
-				<option value="mercedes">Mercedes</option>
-				<option value="audi">Audi</option>				
+		  	<select value={option} onChange={handleChange} >
+				{ optionCities }			
 			</select>
 		</div>
 	);
